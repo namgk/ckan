f5edc000e95cbbbe6455a73714c7580500b004fb
Nam Giang <kyng@ece.ubc.ca>
2018-11-05 16:30:26 -0800

    start again

--- runtest.sh
+++ runtest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nosetests --ckan --with-pylons=test-core-2.8.ini ckanext/timeseries/tests/test_create_short.py
+nosetests --ckan --with-pylons=test-core-2.8.ini ckanext/timeseries/tests/test_create.py
